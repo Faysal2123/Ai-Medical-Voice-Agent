3c8eb9df88d21019e8d49c2b000f763ed41288e8
Faysal2123 <foysal2005n1@gmail.com>
2025-09-18 13:54:35 +0600

    Setup project and create landing screen design

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import HeroSectionOne from "@/components/hero-section-demo-1";
+import { BentoGridThirdDemo } from "./_components/FeatureBentoGrid";
+
+
+export default function Home() {
+  return (
+   <div>
+    <HeroSectionOne></HeroSectionOne>
+    <BentoGridThirdDemo></BentoGridThirdDemo>
+   </div>
+  );
+}
